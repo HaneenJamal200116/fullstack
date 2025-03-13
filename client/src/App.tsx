5453ad8262ffcbee87ca59597b725a6e4c096830
HaneenJamal200116 <haneenalsalman06@gmail.com>
2025-03-13 03:50:24 +0300

    docker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 import Navbar from './sections/Navbar'
 import AllProducts from './sections/AllProducts'
 
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import TechProducts from './sections/TechProducts'
 import ClothingProducts from './sections/ClothesProducts'
 import ProductDetail from './sections/ProductDetails'
@@ -23,7 +23,7 @@ function App() {
       <Router>
       <Navbar /> 
       <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Navigate to="/category/all" replace />} />
       <Route path="/category/all" element={<AllProducts />} />
       <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/category/tech" element={<TechProducts />} />
