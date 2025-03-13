90537d7c1f10380a7a5782e29dd7da36991d0e51
HaneenJamal200116 <haneenalsalman06@gmail.com>
2025-03-13 22:34:46 +0300

    docker

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 export const client = new ApolloClient({
   uri: "https://backend-hj.great-site.net/graphql",
+  cache: new InMemoryCache(),
 });
 
 export const GET_CATEGORIES = gql`
