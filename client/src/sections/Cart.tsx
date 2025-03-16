2043d12b3a43df936e396daaa3acdc540d2de9cb
HaneenJamal200116 <haneenalsalman06@gmail.com>
2025-03-16 16:20:55 +0300

    cart-btn

--- client/src/sections/Cart.tsx
+++ client/src/sections/Cart.tsx
@@ -103,13 +103,13 @@ const Cart: React.FC<CartProps> = ({ isOpen, onClose }) => {
 
 
   return (
-      <div className="fixed top-[88px] inset-0 flex justify-end z-50 ">
+      <div className="fixed top-[88px] inset-0 flex justify-end z-50 "data-testid="cart-overlay">
         
         {/* Overlay */}
         <div className="absolute inset-0 bg-black/10 " onClick={onClose}></div>
 
         {/* Modal Content */}
-        <div className="relative z-10 bg-white p-6  max-h-[500px] overflow-auto  shadow-lg w-[400px] right-15" data-testid="cart-overlay">
+        <div className="relative z-10 bg-white p-6  max-h-[500px] overflow-auto  shadow-lg w-[400px] right-15" >
           {/* Close Button */}
           <button
             className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 cursor-pointer"
