b6dd6165936fedd8cc8163fb13b309261cb2734d
HaneenJamal200116 <haneenalsalman06@gmail.com>
2025-03-16 18:08:31 +0300

    cart-overlay test 10

--- client/src/sections/Cart.tsx
+++ client/src/sections/Cart.tsx
@@ -103,8 +103,8 @@ const Cart: React.FC<CartProps> = ({ isOpen, onClose }) => {
 
 
   return (
-    
-    <div className="fixed top-[88px] inset-0 flex justify-end z-50 "data-testid="cart-overlay">
+    <div data-testid="cart-overlay">
+        <div className="fixed top-[88px] inset-0 flex justify-end z-50 ">
         
         {/* Overlay */}
         <div className="absolute inset-0 bg-black/10 " onClick={onClose}></div>
@@ -229,6 +229,8 @@ const Cart: React.FC<CartProps> = ({ isOpen, onClose }) => {
          
         </div>
     </div>
+    </div>
+   
     )
   }
       
